7a768315b4c9db0fc41aa56b0d00534e52282e38
Gabriel Mello <gabrielmellocode@gmail.com>
2025-05-31 22:40:58 -0300

    rosas 🌹

--- 01-variables/src/01-variables.kt
+++ 01-variables/src/01-variables.kt
@@ -1,4 +1,5 @@
 fun main() {
+    // 01 - Variables
     var userName: String = "joão"
     userName = "gabriel"
 
@@ -7,5 +8,52 @@ fun main() {
 
     println("hello $userName! your age is $userAge.")
 
-    
+    // Integer
+    val number = 123
+
+    val integerMaxValue = Int.MAX_VALUE
+    println("integer max value: $integerMaxValue")
+
+    val integerMinValue = Int.MIN_VALUE
+    println("integer min value: $integerMinValue")
+
+    // Byte
+    val byte: Byte = 64
+
+    val byteMaxValue: Byte = Byte.MAX_VALUE
+    println("byte max value: $byteMaxValue")
+
+    val byteMinValue: Byte = Byte.MIN_VALUE
+    println("byte min value: $byteMinValue")
+
+    // Short
+    val short: Short = 1
+
+    val shortMaxValue: Short = Short.MAX_VALUE
+    println("short max value: $shortMaxValue")
+
+    val shortMinValue: Short = Short.MIN_VALUE
+    println("short min value: $shortMinValue")
+
+    // Long
+    val long: Long = 987654321
+
+    val longMaxValue: Long = Long.MAX_VALUE
+    println("long max value: $longMaxValue")
+
+    val longMinValue: Long = Long.MIN_VALUE
+    println("long min value: $longMinValue")
+
+    // Float & Double
+    val float = 2.5F
+    val double = -12.5
+
+    println("float = $float and double = $double")
+
+    // Char & Boolean
+    val char = 'C'
+    println("char = $char")
+
+    val boolean = true
+    println("boolean = $boolean")
 }
