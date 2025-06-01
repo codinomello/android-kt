e439248b0da66b7e81104cac169d4503d8aa4f9c
Gabriel Mello <gabrielmellocode@gmail.com>
2025-05-31 22:14:20 -0300

    var 🎣

---
+++ 01-variables/src/01-variables.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var userName: String = "joão"
+    userName = "gabriel"
+
+    var userAge: Int = 25
+    userAge = 16
+
+    println("hello $userName! your age is $userAge.")
+
+    
+}
--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,3 @@
 fun main() {
-    val name = "Kotlin"
-
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-
-        println("i = $i")
-    }
+    println("hello, world!")
 }
